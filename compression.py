77a982366e93ed3f3f0a37fad905bb93ed5c117b
mai <huu.mai@telecom-paris.fr>
2021-06-22 10:39:25 +0200

    compression

---
+++ compression.py
@@ -0,0 +1,30 @@
+def xor(u, v):
+    return 0
+
+def and(u, v):
+    return 0
+
+def compression(chunk, hv):
+
+    # hv is the hash value. hv[0] = hv0, ..., hv[7] = hv7.
+    hv0 = hv[0]
+    hv1 = hv[1]
+    hv2 = hv[2]
+    hv3 = hv[3]
+    hv4 = hv[4]
+    hv5 = hv[5]
+    hv6 = hv[6]
+    hv7 = hv[7]
+
+    # chunk is a bit array of 256 bits, cut in 8 words of 32 bits.
+    a = chunk[0:4]
+    b = chunk[4:8]
+    c = chunk[8:12]
+    d = chunk[12:16]
+    e = chunk[16:20]
+    f = chunk[20:24]
+    g = chunk[24:28]
+    h = chunk[28:32]
+
+
+
