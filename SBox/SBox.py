0024e3c535d3b4816a21bbade69de1e8e0fd77e6
lysiane-burel <77341053+lysiane-burel@users.noreply.github.com>
2021-06-23 16:01:56 +0200

    Adaptation à toutes les tailles

--- SBox/SBox.py
+++ SBox/SBox.py
@@ -1,19 +1,24 @@
 import random
-from FHLVa import FHLVa
+from FHL import FHL
 import time
 
 
-nbSymb = 16
-#X = (np.random.randn(1, NbSymboles)).sort()
+nbSymb = int(input("Entrez le nombre de symboles. Exemple : 16\n"))
+
+
 symbPerm = [random.randint(0, 16) for k in range(nbSymb)]
 symbPerm.sort()
-longDep = 4
 
-print("Entrez le vecteur de départ de taille 4. Exemple : 0 1 2 3")
+
+#symbPerm = [3, 15, 6, 5, 7, 13, 4, 8, 9, 1, 11, 10, 16, 12, 2, 14]
+longDep = int(nbSymb/4)
+
+print("Entrez le vecteur de départ de taille ", longDep, ". Exemple : ", " ".join(f"{k:d}" for k in range(longDep)), sep = '', end = '')
+
 vectDep = [int(s) for s in input().split()]
 
 t=time.perf_counter()
-#print(vectDep, len(vectDep), type(vectDep))
+
 if len(vectDep)!=longDep:
     print("Mauvaise longueur de vecteur")
     exit
@@ -22,7 +27,8 @@ for i in range(longDep):
     j = (2*i+3)*vectDep[i] + i+1
     nouvVect.append(symbPerm[j%nbSymb])
 
-vectSortie = [elem%nbSymb for elem in FHLVa(nouvVect)]
+vectSortie = [elem%nbSymb for elem in FHL(nouvVect)]
+print(vectSortie)
 
 print("\nVecteur de sortie : 0x"+ "".join(f"{elem:x}" for elem in vectSortie))
 print("Temps d'exécution :", round((time.perf_counter()-t)*1000, 3), "ms")
