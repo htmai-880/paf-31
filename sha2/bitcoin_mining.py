c8b184219d6a7fa16948478a52e536ae53a2b7b9
chanaron <louis.chanaron@telecom-paris.fr>
2021-06-28 10:17:06 +0200

    compteur

--- sha2/bitcoin_mining.py
+++ sha2/bitcoin_mining.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 from sha2 import *
 import random as rd
 import string
+import time
 
 length_block = 1000
 ints = string.digits
-block = int('1'+''.join(rd.choice(ints) for _ in range(length_block)))
+block = int('1'+''.join(rd.choice(ints) for _ in range(length_block-1)))
 
 def mine(dif_target):
     nounce = 0
@@ -18,6 +19,19 @@ def mine(dif_target):
 
 def pickaxe(nounce):
     return sha256(sha256(block*2**32+nounce, typ = 'int'), typ = 'int')
+
+def mining_perf(dif_min):
+    D = []
+    t_list = []
+    for dif_target in range(dif_min, 256):
+        D.append(dif_target)
+        t = time.perf_counter()
+        mine(2**dif_target)
+        dt = round((time.perf_counter()-t)*1000, 3)
+        t_list.append(dt)
+    plt.plot(D, t_list, 'o')
+    plt.show()
+        
 
 if __name__ == "__main__":
     d = int(input("Entrer le log2 du facteur de difficulté du bloc [0, 255]:\n"))
