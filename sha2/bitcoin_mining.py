3f84edd160ff82cc81a9a3267c4e89b18b7917b5
chanaron <louis.chanaron@telecom-paris.fr>
2021-06-28 10:25:53 +0200

    affichage

--- sha2/bitcoin_mining.py
+++ sha2/bitcoin_mining.py
@@ -6,29 +6,33 @@ import time
 
 length_block = 1000
 ints = string.digits
-block = int('1'+''.join(rd.choice(ints) for _ in range(length_block-1)))
+def_block = int('1'+''.join(rd.choice(ints) for _ in range(length_block-1)))
 
-def mine(dif_target):
+def mine(dif_target, block = def_block):
     nounce = 0
     while(nounce < 2**256):
-        if pickaxe(nounce) <= dif_target:
+        if pickaxe(nounce, block) <= dif_target:
             print(nounce)
             return nounce
         nounce += 1
     return
 
-def pickaxe(nounce):
+def pickaxe(nounce, block):
     return sha256(sha256(block*2**32+nounce, typ = 'int'), typ = 'int')
 
-def mining_perf(dif_min):
+def mining_perf(dif_min, num_block):
     D = []
     t_list = []
     for dif_target in range(dif_min, 256):
-        D.append(dif_target)
-        t = time.perf_counter()
-        mine(2**dif_target)
-        dt = round((time.perf_counter()-t)*1000, 3)
-        t_list.append(dt)
+        for k in range(num_block):
+            block = int('1'+''.join(rd.choice(ints) for _ in range(length_block-1)))
+
+            D.append(dif_target)
+            t = time.perf_counter()
+            mine(2**dif_target, block)
+            dt = round((time.perf_counter()-t)*1000, 3)
+            t_list.append(dt)
+        
     plt.plot(D, t_list, 'o')
     plt.show()
         
