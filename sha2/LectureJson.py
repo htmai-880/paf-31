91915bb11f851142d99182ae93a1c5acb06340af
chanaron <louis.chanaron@telecom-paris.fr>
2021-06-30 15:14:42 +0200

    lecture de json fonctionnelle

--- sha2/LectureJson.py
+++ sha2/LectureJson.py
@@ -3,31 +3,31 @@ from sha2 import sha256
 from binascii import unhexlify, hexlify
 import hashlib
 from struct import pack, unpack, unpack_from
+from codecs import encode
 
-def lectureJson(bitcoin_num):
+def block_hash(bitcoin_num):
     with open(str(bitcoin_num) + ".json", "r") as f:        
         blocJson = json.loads("".join(f.readlines()))
         
-        ver = pack('<I', blocJson['blocks'][0]['ver']).encode('hex_codec')
+        ver = pack('<I', blocJson['blocks'][0]['ver']).hex()
         
-        prev_nounce = blocJson['blocks'][0]['prev_block']
-        prev_nounce = prev_nounce.decode('hex')[::-1].encode('hex_codec')
+        prev_nounce = unhexlify(blocJson['blocks'][0]['prev_block'])
+        prev_nounce = prev_nounce[::-1].hex()
         
-        mrkl_root = blocJson['blocks'][0]['mrkl_root']
-        mrkl_root = mrkl_root.decode('hex')[::-1].encode('hex_codec')
+        mrkl_root = unhexlify(blocJson['blocks'][0]['mrkl_root'])
+        mrkl_root = mrkl_root[::-1].hex()
         
-        time = pack('<I', blocJson['blocks'][0]['time']).encode('hex_codec')
+        time = pack('<I', blocJson['blocks'][0]['time']).hex()
         
-        nbits = pack('<I', blocJson['blocks'][0]['bits']).encode('hex_codec')
+        nbits = pack('<I', blocJson['blocks'][0]['bits']).hex()
         
-        nonce = pack('<I', blocJson['blocks'][0]['nonce']).encode('hex_codec') 
+        nonce = pack('<I', blocJson['blocks'][0]['nonce']).hex() 
         
         header_hex = ver + prev_nounce + mrkl_root + time + nbits + nonce
-        header_bin = header_hex.decode('hex')    
-        #a=int('0x'+bloc, 0)
-        return header_bin
-    
-header = lectureJson(125552)
+        header_bin = int(header_hex,16)
+        diggest = sha256(sha256(header_bin, typ = 'int'), typ = 'int')
+        
+        return unhexlify(hex(diggest)[2:])[::-1].hex()
 
-hash = hashlib.sha256(hashlib.sha256(header).digest()).digest()
-print(hexlify(hash).decode("utf-8"))
+if __name__ == "__main__":
+    print(block_hash(125552))
